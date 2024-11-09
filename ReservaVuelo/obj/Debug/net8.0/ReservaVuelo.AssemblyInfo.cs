8a805cbd01ed1dfc7c169b70247ed956a8003a42
Shari <shariloza501@gmail.com>
2024-11-09 11:43:32 -0400

    lol

--- ReservaVuelo/obj/Debug/net8.0/ReservaVuelo.AssemblyInfo.cs
+++ ReservaVuelo/obj/Debug/net8.0/ReservaVuelo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReservaVuelo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+121e6152c9f303082ca312b68ee6741c749dd031")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de995fcffdce02000224465bb69672f4a5e13546")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReservaVuelo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReservaVuelo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
